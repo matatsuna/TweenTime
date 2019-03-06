0028232d998d17de031b019e810ee858b37ece21
matatsuna <matatsuna@gmail.com>
2019-03-06 17:23:51 +0900

    fix Vector3

---
+++ src/scripts/editor/PropertyVector3.js
@@ -0,0 +1,87 @@
+import 'jquery';
+import PropertyBase from './PropertyBase';
+let DraggableNumber = require('draggable-number.js');
+
+let tpl_property = require('../templates/PropertyVector3.tpl.html');
+
+export default class PropertyVector3 extends PropertyBase {
+    constructor(instance_property, lineData, editor, key_val = false) {
+        super(instance_property, lineData, editor, key_val);
+        this.onInputChange = this.onInputChange.bind(this);
+        this.$input = this.$el.find('input');
+    }
+
+    render() {
+        super.render();
+        // By default assign the property default value
+        var val = this.getCurrentVal();
+
+        var data = {
+            id: {
+                x: this.instance_property.name + "X",
+                y: this.instance_property.name + "Y",
+                z: this.instance_property.name + "Z",
+            }, // "circleRadius" instead of "circle radius"
+            label: this.instance_property.label || this.instance_property.name,
+            val: val
+        };
+
+        var view = tpl_property(data);
+        this.$el = $(view);
+
+        var $input = this.$el.find('input');
+
+        var onChangeEnd = () => {
+            this.editor.undoManager.addState();
+        };
+
+        var draggableOptions = {
+            changeCallback: () => this.onInputChange(),
+            endCallback: () => onChangeEnd()
+        };
+
+        // Set min & max if they are defined.
+        if ('min' in this.instance_property) {
+            draggableOptions.min = this.instance_property.min;
+        }
+        if ('max' in this.instance_property) {
+            draggableOptions.max = this.instance_property.max;
+        }
+
+        var draggable = new DraggableNumber($input[0], draggableOptions);
+        $($input[0]).data('draggable', draggable);
+        $($input[0]).change(this.onInputChange);
+
+        var draggable = new DraggableNumber($input[1], draggableOptions);
+        $($input[1]).data('draggable', draggable);
+        $($input[1]).change(this.onInputChange);
+
+        var draggable = new DraggableNumber($input[2], { dragThreshold: 0.1 });
+        $($input[2]).data('draggable', draggable);
+        $($input[2]).change(this.onInputChange);
+    }
+
+    remove() {
+        super.remove();
+        if (this.$input.data('draggable')) {
+            this.$input.data('draggable').destroy();
+        }
+        delete this.$el;
+        delete this.$input;
+    }
+
+    update() {
+        super.update();
+        // super.render();
+        var val = this.getCurrentVal();
+
+        var draggable = this.$input.data('draggable');
+
+        if (draggable) {
+            draggable.set(val.toFixed(3));
+        }
+        else {
+            this.$input.val(val.toFixed(3));
+        }
+    }
+}
